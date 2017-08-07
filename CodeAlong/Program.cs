d9dee4f8da89950d704f997eb58fbc1f974c6146
camschoch <camschoch3@gmail.com>
2017-08-07 07:20:03 -0500

    first commit

---
+++ CodeAlong/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            payCalculator runCalculator = new payCalculator();
+            runCalculator.hoursWorked = runCalculator.getHoursWorked();
+            runCalculator.payRate = runCalculator.getPayRate();
+            runCalculator.getTotalPay(runCalculator.hoursWorked, runCalculator.payRate);
+
+        }
+    }
+}
---
+++ CodeAlong/payCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlong
+{
+    class payCalculator
+    {
+        public double hoursWorked;
+        public double payRate;
+        public double totalPay;
+        public double getHoursWorked()
+        {
+            Console.WriteLine("please enter youre hours worked.");
+            hoursWorked = double.Parse(Console.ReadLine());
+            return hoursWorked;
+        }
+        public double getPayRate()
+        {
+            Console.WriteLine("please enter your pay rate");
+            payRate = double.Parse(Console.ReadLine());
+            return payRate;
+        }
+        public void getTotalPay(double hoursWorked, double payRate)
+        {
+            totalPay = payRate * hoursWorked;
+            Console.WriteLine("Your totla pay: " + totalPay);
+            Console.ReadLine();
+        }
+    }
+}
